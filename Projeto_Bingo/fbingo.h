d10f9303b851d8ce7b51afa6fd4539b7d33c2212
lidymonteirowm <lidianedesouzamonteiro@gmail.com>
2015-12-13 01:15:48 -0300

    projeto parcial - backup 12/12

--- Projeto_Bingo/fbingo.h
+++
@@ -1,8 +0,0 @@
-/* 
-** Cabeçalho das funções implementadas no módulo fbingo.c
-*/
-
-/* 
-** Função que exibe os números sorteados e na ordem crescente 
-*/
-void exibe_sorteados(int num_sorteado[], int qtd_cart);
