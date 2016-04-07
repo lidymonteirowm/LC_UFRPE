147cc9fafd0832bcb5b76334bc83e71c5453e5a8
lidymonteirowm <lidianedesouzamonteiro@gmail.com>
2016-04-07 01:43:23 -0300

    1. Exercicios com Inteiros - P1.2

--- 2o_Periodo/Algoritmos_Estrutura_de_Dados/Carderninho/12.c
+++ 2o_Periodo/Algoritmos_Estrutura_de_Dados/Carderninho/12.c
@@ -1,8 +1,16 @@
+/*Programa lê um inteiro positivo n e imprime o valor da soma:
+1 + 2 + 3 + ... + n
+Uma solução alternativa é calcular a soma da PA com razão 1:
+soma = (n * (n + 1)) / 2
+*/
+
 #include <stdio.h>
-int main(){
+void main(){
   int n, soma=0, i;
+  printf("Digite um inteiro positivo: ");
+  scanf("%d", &n);
   for(i=1; i<=n; i++){
     soma = soma + i;
   }
-  print("Soma dos %d primeiros inteiros positivos: %d", n, soma);
+  printf("\nSoma dos %d primeiros inteiros positivos: %d\n", n, soma);
 }
