8831e553e4aa7101f6eb1f0ca6e32a399a99e4c6
lidymonteirowm <lidianedesouzamonteiro@gmail.com>
2016-04-07 01:32:26 -0300

    1. Exercicios com Inteiros - P1.1

---
+++ 2o_Periodo/Algoritmos_Estrutura_de_Dados/Carderninho/11.c
@@ -0,0 +1,14 @@
+/* Programa lê uma sequência de números inteiros
+  não nulos, seguida por zero para indicar o fim da sequência,
+  e exibe o quadrado de cada número da sequência. */
+#include <stdio.h>
+int main(){
+  int num;
+  printf("Digite uma sequencia de numeros inteiros, nao-nula, seguida por zero: \n");
+  scanf("%d", &num);
+  while(num!=0){
+    printf("\n%d ao quadrado: %d \n", num, num*num);
+    scanf("%d", &num);
+  }
+  return 0;
+}
---
+++ 2o_Periodo/Algoritmos_Estrutura_de_Dados/Carderninho/12.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+  int n, soma=0, i;
+  for(i=1; i<=n; i++){
+    soma = soma + i;
+  }
+  print("Soma dos %d primeiros inteiros positivos: %d", n, soma);
+}
